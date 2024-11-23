626899997f2ccd8c2ab591ccd4d1c3076448fcd0
Balbito <omar.ibanez343@gmail.com>
2024-11-23 01:41:19 -0600

    feat: Development of Columns and Card components, alogn with SCSS styles

---
+++ src/components/TaskList.vue
@@ -0,0 +1,63 @@
+<template>
+<ul>
+    <li
+        v-for="(task, index) in tasks"
+        :key="index"
+        :class="{ completed: task.completed }">
+        {{ task.title }}
+    </li>
+
+    <input type="text"
+        placeholder="Añadir nueva tarea"
+        v-model="title"
+        @keyup.enter="add()"
+    >
+</ul>
+</template>
+
+<script>
+const TaskList = {
+    name: 'TaskList',
+
+    props: {
+        listId: String,
+        tasks: Array
+    },
+
+    data() {
+        return {
+            title: ''
+        }
+    },
+
+    methods: {
+        add() {
+            // TODO
+        }
+    }
+}
+
+export default TaskList;
+</script>
+
+<style lang="scss" scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+li {
+    background-color: #fAFAFA;
+    border-radius: 3px;
+    border-bottom: 1px solid #CCC;
+    margin: 0.25rem 0;
+    padding: 1rem;
+
+    &.completed {
+        background-color: #CFD8DC;
+        color: #90A4AE;
+    }
+}
+
+</style>
--- src/components/views/Board.vue
+++ src/components/views/Board.vue
@@ -6,21 +6,55 @@
             Mis Paneles > {{ name }}
         </span>
     </h3>
-    <div>
-        ...
+    <input 
+        type="text"
+        placeholder="Añade una lista"
+        v-model="listName"
+        @keyup.enter="add()"
+    />
+
+    <div class="container">
+        <column
+            v-for="(list, index) in boardList"
+            :key="index"
+            :listId="list.id"
+            :name="list.name"
+        >
+        
+        </column>
     </div>
 </section>
 
 </template>
 
 <script>
-
+import Column from '@/components/Column'
 const Board = {
     name: 'board',
-    components: {},
+    components: {Column},
     props: {
         name: String,
-        id: Number
+        id: String
+    },
+
+    data() {
+        return {
+            listName: '',
+            boardList: [
+                {
+                    id: '1', name: 'Todo'
+                },
+                {
+                    id: '2', name: 'Doing'
+                }
+            ]
+        }
+    },
+
+    methods: {
+        add() {
+            this.boardList.push({ name: this.listName })
+        }
     }
 }
 
@@ -38,8 +72,28 @@ export default Board;
         margin: 1.5rem;
     }
 
+    input {
+        background-color: #607D8B;
+        border: 0;
+        border-radius: 3px;
+        box-shadow: 0 0 0 0.5px rgba(49,49,93,.03),
+        0 2px 5px 0 rgba(49,49,93,.1),
+        0 1px 2px 0 rgba(0,0,0,.08);
+        color: #f7fcff;
+        font-size: 1.2rem;
+        margin: 0rem;
+        outline: 0;
+        padding: 1rem;
+        transition: all 600ms ease;
+        
+        &:active,
+        &:focus {
+            background-color: #FAFAFA;
+        }
+        
+    }
+    
     span {
         color: #546e7a;
     }
-
 </style>
